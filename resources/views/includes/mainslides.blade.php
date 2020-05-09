ec6048d4f0b4273037189d76e7391c0fa5e1eba2
aungtnaing82 <aungtnaing82@gmail.com>
2020-05-09 14:20:14 +0630

    new d

--- resources/views/includes/mainslides.blade.php
+++ resources/views/includes/mainslides.blade.php
@@ -12,7 +12,7 @@
                         <div class="text-content">
                             <h1 class="title1">{{ $mainslide[0]->mtitle }}</h1>
                             <p class="myanmar3">{{ $mainslide[0]->mdescription }}</p>
-                            <p class="myanmar3"><b><u>Our new branding service(ခေတ်ပညာ)</u></b></p>
+                            <p class="myanmar3"><b><u>Our new education online learning platform(ခေတ်ပညာ)</u></b></p>
                             <p class="myanmar3">ခေတ်ပညာ(Khit Pyinnyar) သည် မြန်မာနိုင်ငံ တစ်ဝှမ်းရှိ မိဘ ပြည်သူများ အတွက် ခေတ်နှင့် အညီ လိုအပ်သော ပညာရပ်များကို နေရာမရွေး ၊အချိန်မရွေး၊ အသက်ရွယ်မရွေး လေ့လာဆည်းပူး နို်င်ရန် ရည်ရွယ်ချက်ဖြင့် တည်ဆောက်ထားခြင်း ဖြစ်ပါသည်။ ခေတ်ပညာ(Khit Pyinnyar)ကိုလေ့လာသင်ယူခြင်းအားဖြင့် အသိပညာတစ်ခု အတတ်ပညာတစ်ခု ရရှိသွားလျှင်ပင် ခေတ်ပညာ နေဖြင့် ဝမ်းမြောက်ဂုဏ်ယူနေမည်ဖြစ်သည်။ </p>
                             <div class="banner-button">
                                 <a class="default-btn button" href="https://khitpyinnyar.com">Learn More..</a>                 
